5c4e5434d084e93ba7e1680d25e2ba8fc20789c1
vedan200 <nguyendat111195@gmail.com>
2024-06-07 22:40:55 +0700

    Test opengl

--- OpenGL/project1Test/src/main.cpp
+++ OpenGL/project1Test/src/main.cpp
@@ -4,14 +4,36 @@
 
 using namespace std;
 
-int main(){
+void framebuffer_size_callback(GLFWwindow * window, int width, int height){
+    glViewport(0, 0, width, height);
+}
 
-    GLFWwindow * window;
+void processInput(GLFWwindow * window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+        glfwSetWindowShouldClose(window, true);
+        cout << "CLOSE" << endl;
+    }
+    
+}
+
+int main(){
+    const int screen_width = 1280;
+    const int screen_height = 720;
     if(!glfwInit()){
         return -1;
     }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow * window;
+    
 
-    window = glfwCreateWindow(640, 480, "Window!", NULL, NULL);
+    window = glfwCreateWindow(screen_width, screen_height, "Window!", NULL, NULL);
+    if(window == NULL){
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
     glfwMakeContextCurrent(window);
 
 
@@ -20,8 +42,10 @@ int main(){
         glfwTerminate();
         return -1;
     }
-    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     while(!glfwWindowShouldClose(window)){
+        processInput(window);
         glfwPollEvents();
         glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
